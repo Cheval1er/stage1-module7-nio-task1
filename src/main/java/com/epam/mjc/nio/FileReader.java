7158b6eb89623a14b1bf598cf1029a6a6daf2cb1
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2024-01-12 12:01:50 +0400

    Task 1 File uploading module7

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,43 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import com.epam.mjc.nio.Profile;
 
+import java.io.File;
+import java.io.BufferedReader;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            if (file.exists()) {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    String key = line.split(": ")[0];
+                    String value = line.split(": ")[1];
+
+                    switch (key) {
+                        case "Name":
+                            profile.setName(value);
+                            break;
+                        case "Age":
+                            profile.setAge(Integer.parseInt(value));
+                            break;
+                        case "Email":
+                            profile.setEmail(value);
+                            break;
+                        case "Phone":
+                            profile.setPhone(Long.parseLong(value));
+                            break;
+                        default:
+                            new Profile();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
